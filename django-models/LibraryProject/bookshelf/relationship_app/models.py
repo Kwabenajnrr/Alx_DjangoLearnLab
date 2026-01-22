d4db9ed550ebf9bc090e050785fa5a70c1f54d0c
David Oduro Aboagye <your-github-email@example.com>
2026-01-22 14:15:48 +0000

    Add relationship_app with advanced model relationships

--- django-models/LibraryProject/bookshelf/relationship_app/models.py
+++ django-models/LibraryProject/bookshelf/relationship_app/models.py
@@ -1,25 +1,22 @@
 from django.db import models
 
-
 class Author(models.Model):
     name = models.CharField(max_length=100)
 
-    
     def __str__(self):
         return self.name
-
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    
+
     def __str__(self):
         return self.title
 
 
 class Library(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
 
     def __str__(self):
@@ -32,4 +29,3 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
-
