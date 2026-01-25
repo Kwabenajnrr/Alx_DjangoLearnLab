595003b2e78b48a02dc624b63e52b8ee93044a15
David Oduro Aboagye <your-github-email@example.com>
2026-01-25 17:43:28 +0000

    initial commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,15 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Library
 
 
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"  # 👈 REQUIRED
     context_object_name = "library"
-
-
-
 
 
 def list_books(request):
