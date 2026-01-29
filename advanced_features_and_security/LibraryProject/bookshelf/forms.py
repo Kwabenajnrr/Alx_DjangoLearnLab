b996321751132dc06cb3ae68d8db8bba98d5089a
David Oduro Aboagye <your-github-email@example.com>
2026-01-29 16:17:09 +0000

    first commit

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,10 +1,5 @@
 from django import forms
-from .models import Book
 
-class BookForm(forms.ModelForm):
-    class Meta:
-        model = Book
-        fields = ['title', 'author', 'published_date']  # Include fields you want to allow users to edit
-        widgets = {
-            'published_date': forms.DateInput(attrs={'type': 'date'}),
-        }
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=100)
+    description = forms.CharField(widget=forms.Textarea, required=False)
