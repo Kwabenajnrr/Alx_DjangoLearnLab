346844856d4a7417b9ee2ee6802b5c47d9abf21e
David Oduro Aboagye <your-github-email@example.com>
2026-02-06 02:07:56 +0000

    Api commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,8 @@
-from rest_framework.generics import ListCreateAPIView
+from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
 
-class BookList(ListCreateAPIView):
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
