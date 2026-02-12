cdccc245d8a5bf01593e5cddff970374bfa87150
David Oduro Aboagye <your-github-email@example.com>
2026-02-12 13:09:50 +0000

    Implemented user authentication system

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -16,6 +16,7 @@ from .views import (
 
 
 "post/<int:pk>/delete/", "post/<int:pk>/update/", "post/new/"
+"comment/<int:pk>/update/", "post/<int:pk>/comments/new/", "comment/<int:pk>/delete/"
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register_view, name='register'),
