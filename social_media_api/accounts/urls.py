c584932ebb7d994457be3d29662763832bc13142
David Oduro Aboagye <your-github-email@example.com>
2026-03-01 17:08:31 +0000

    second commit

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from .views import RegisterView, LoginView, ProfileView
 from rest_framework.authtoken import views as drf_views
+from .views import follow_user, unfollow_user
+
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
     path('api-token-auth/', drf_views.obtain_auth_token, name='api_token_auth'),
+    path('follow/<int:user_id>/', follow_user),
+    path('unfollow/<int:user_id>/', unfollow_user),
 ]
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -6,6 +6,49 @@ from .models import CustomUser
 from .serializers import UserSerializer, RegisterSerializer, LoginSerializer
 from django.contrib.auth import authenticate
 
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model
+from rest_framework import status
+
+User = get_user_model()
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def follow_user(request, user_id):
+    try:
+        user_to_follow = User.objects.get(id=user_id)
+
+        if user_to_follow == request.user:
+            return Response({"error": "You cannot follow yourself"}, status=400)
+
+        request.user.following.add(user_to_follow)
+        return Response({"message": "User followed successfully"}, status=200)
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def unfollow_user(request, user_id):
+    try:
+        user_to_unfollow = User.objects.get(id=user_id)
+
+        request.user.following.remove(user_to_unfollow)
+        return Response({"message": "User unfollowed successfully"}, status=200)
+
+    except User.DoesNotExist:
+        return Response({"error": "User not found"}, status=404)
+
+
+
+
+
+
+
 class RegisterView(generics.CreateAPIView):
 	queryset = CustomUser.objects.all()
 	serializer_class = RegisterSerializer
