e5be05123d500f26de4cc54b1fb4fa468fdf9430
David Oduro Aboagye <your-github-email@example.com>
2026-03-01 17:12:47 +0000

    second commit

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,46 +1,18 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter
-
-
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('-created_at')
-    serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-    filter_backends = [SearchFilter]
-    search_fields = ['title', 'content']
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CommentViewSet(viewsets.ModelViewSet):
-    queryset = Comment.objects.all().order_by('-created_at')
-    serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework import permissions
 from .models import Post
 from .serializers import PostSerializer
 
 
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def feed(request):
-    user = request.user
-    following_users = user.following.all()
+    following_users = request.user.following.all()
 
-    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
+    posts = Post.objects.filter(
+        author__in=following_users
+    ).order_by('-created_at')
 
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
